140a9be7a49a996baf7a165723b1842bd70b9464
sipity19 <sipity19@gmqil.com.com>
2019-11-04 13:27:55 +0800

    Config updated, several formatting changes auto applied

--- functions/utils/notifications.js
+++ functions/utils/notifications.js
@@ -1,67 +1,26 @@
 const admin = require('firebase-admin')
-const nodemailer = require('nodemailer')
+try { admin.initializeApp() } catch (e) { console.log(e) }
 
 module.exports = {
-  notifyUser: async (userUid, payload) => {
+  notifyUser: (userUid, payload) => {
     console.log(userUid, payload)
 
-    const snapshot = await admin
-      .database()
-      .ref(`/notification_tokens/${userUid}`)
-      .once('value')
+    return admin.database().ref(`/notification_tokens/${userUid}`).once('value').then(snapshot => {
+      let registrationTokens = []
 
-    let registrationTokens = []
-
-    if (snapshot.exists()) {
       snapshot.forEach(token => {
         if (token.val()) {
           registrationTokens.push(token.key)
         }
       })
-    }
 
-    if (registrationTokens.length) {
-      await admin.messaging().sendToDevice(registrationTokens, payload)
-    } else {
-      /*
-      if (functions.config().email_notifications) {
-        const emailNotificationSnap = admin()
-          .database()
-          .ref(`email_notifications/${userUid}`)
-          .once('value')
-
-      
-        if (emailNotificationSnap.exists()) {
-          const userSnap = await admin
-            .database()
-            .ref(`users/${userUid}`)
-            .once('value')
-
-          if (userSnap.exists()) {
-            const { email = false } = userSnap.val()
-            const { body = '', title = '', click_action = '' } = payload
-            const mailTransport = nodemailer.createTransport(functions.config().email_notifications.transport)
-
-            if (email) {
-              const mailOptions = {
-                to: email,
-                subject: title,
-                text: `${body}
-                ${click_action}`
-              }
-
-              await mailTransport.sendMail(mailOptions)
-              console.log('Notification email send!')
-            }
-          }
-        }
+      if (registrationTokens.length) {
+        return admin.messaging().sendToDevice(registrationTokens, payload)
+      } else {
+        console.log('Not tokens registered')
+        return null
       }
-      */
-
-      console.log('Not tokens registered')
-      return null
-    }
-
-    return 1
+    })
   }
+
 }
